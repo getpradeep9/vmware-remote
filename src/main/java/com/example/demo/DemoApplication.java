0198c2934f5172908d9cbd9974a50fe91f5e069f
pradeep <pradeep@abc.com>
2022-01-25 06:54:17 +0530

    today commit v3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("came here ONE");
-		System.out.println("here_to ONE"  );
+		System.out.println("came here ONE TWO");
+		System.out.println("here_to ONE TWO"  );
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
